0c1c5bb3d30c709b4c6ccb8f57e71487b424270a
jiexi <mingjielao@gmail.com>
2021-03-24 21:58:30 -0400

    Modified partialc.ml for the four stages

--- partialc.ml
+++ partialc.ml
@@ -19,19 +19,15 @@ let () =
   Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
   
   let lexbuf = Lexing.from_channel !channel in
-  let ast = Parser.program Scanner.tokenize lexbuf in  
-  let sast = Semant.check ast in
-(*   let LLVM_IR = print_string (Llvm.string_of_llmodule (Codegen.translate sast)) in *)
-  let m = Codegen.translate sast in 
-  Llvm_analysis.assert_valid_module m;
-  print_string (Llvm.string_of_llmodule m)
-(*   match !action with
-    Ast -> print_string (Ast.string_of_program ast)
+  let ast = Parser.program Scanner.tokenize lexbuf in
+  match !action with
+  Ast -> print_string (Ast.string_of_program ast)
   | _ -> let sast = Semant.check ast in
     match !action with
-      Ast     -> ()
-    | Sast    -> print_string (Sast.string_of_sprogram sast)
+      Ast     -> print_string (Ast.string_of_program ast)
+    | Sast    -> () (* We should modified here to print he sast tree*)
     | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate sast))
     | Compile -> let m = Codegen.translate sast in
-	Llvm_analysis.assert_valid_module m;
-	print_string (Llvm.string_of_llmodule m) *)
+  Llvm_analysis.assert_valid_module m;
+  print_string (Llvm.string_of_llmodule m)
+
