89ebb3973d24446ebb3c9fac5b164c5bb2af682c
root <mingjielao@gmail.com>
2021-04-18 21:47:17 +0000

    fixed reassignment bug

--- codegen.ml
+++ codegen.ml
@@ -74,7 +74,7 @@ let translate (functions) =
     in
 
     (* Construct code for an expression; return its value *)
-    let rec expr map builder ((_, e) : sexpr) = match e with
+    let rec expr map builder ((_, expression) : sexpr) = match expression with
 
         SLit i  -> L.const_int i32_t i, map, builder  
       | SFloatLit f -> L.const_float float_t f, map, builder
@@ -82,9 +82,9 @@ let translate (functions) =
       | SStringLit s -> L.build_global_stringptr s "str" builder, map, builder
       | SNoexpr     -> L.const_int i32_t 0, map, builder
       | SId s       -> L.build_load (lookup map s) s builder, map, builder
-      | SAssignOp (v, e) -> let (e', map1, builder) = expr map builder e in match (snd v) with
+      | SAssignOp (v, e) -> let (e', map1, builder) = expr map builder e in (match (snd v) with
                             SId s -> 
-                            ignore(L.build_store e' (lookup map s) builder); e', map1, builder
+                            ignore(L.build_store e' (lookup map s) builder); e', map1, builder)
       | SBinop ((A.Float,_ ) as e1, op, e2) ->
     	  let (e1', _, _) = expr map builder e1
     	  and (e2', _, _) = expr map builder e2 in
@@ -121,12 +121,9 @@ let translate (functions) =
     	  | A.Geq     -> L.build_icmp L.Icmp.Sge
     	  ) e1' e2' "tmp" builder, map, builder
 
-    | SCall ("prints", [e]) -> let e', _, builder = expr map builder e in L.build_call printf_func [| char_format_str ; e' |] "printf" builder, map, builder
+      | SCall ("prints", [e]) -> let e', _, builder = expr map builder e in L.build_call printf_func [| char_format_str ; e' |] "printf" builder, map, builder
 
-    
       | SCall ("printi", [e]) -> let e', _, builder = expr map builder e in L.build_call printf_func [| int_format_str ; e' |] "printf" builder, map, builder
-
-      | _ -> let _ = print_string "gg" in (L.const_int i32_t 0), map, builder
 
       | SCall (f, args) ->
         let (fdef, fdecl) = StringMap.find f function_decls in
