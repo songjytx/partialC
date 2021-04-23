a4dba4f25570530f7a283d7b2aa9655c158f7490
root <js5799@columbia.edu>
2021-04-23 02:53:14 +0000

    minor fix

--- testall.sh
+++ testall.sh
@@ -1,5 +1,5 @@
 # ocamlbuild partialc.native -pkgs llvm,llvm.analysis
-./partialc.native test/test4_while.pc > test.ll
+./partialc.native test/test5_userdefinedfunc_witharg.pc > test.ll
 llc -relocation-model=pic test.ll
 cc -o test.exe test.s
 ./test.exe
