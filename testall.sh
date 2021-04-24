f98964954b42451446e2c9c4e4a697d19fc07859
root <js5799@columbia.edu>
2021-04-24 02:38:34 +0000

    test script

--- testall.sh
+++ testall.sh
@@ -1,5 +1,10 @@
 # ocamlbuild partialc.native -pkgs llvm,llvm.analysis
-./partialc.native test/test5_userdefinedfunc_witharg.pc > test.ll
-llc -relocation-model=pic test.ll
-cc -o test.exe test.s
-./test.exe
+for file in ./test/*.pc; 
+	do
+		echo "=========================="
+		echo $file
+		./partialc.native $file > test.ll
+		llc -relocation-model=pic test.ll
+		cc -o test.exe test.s
+		./test.exe
+	done
